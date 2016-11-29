078cd2a2e8421363ae79ab1ae308590ea29602e2
Jeff Robbins <robbinsjeffreym@gmail.com>
2016-11-29 13:09:41 -0500

    working with children

--- Assets/Editor/BezierCurveInspector.cs
+++ Assets/Editor/BezierCurveInspector.cs
@@ -8,12 +8,55 @@ public class BezierCurveInspector : Editor
     private Transform handleTransform;
     private Quaternion handleRotation;
 
+    private Vector3[] oldPoints;
+    bool start = false;
+
     public override void OnInspectorGUI()
 	{
 		base.OnInspectorGUI();
+        
 	}
 
-    void OnDrawGizmos() {
 
+    void OnSceneGUI() {
+        curve = target as BezierCurve;
+        if (curve.points.Length < 3) {
+            curve.Reset();
+        }
+        
+        if (oldPoints != curve.points) {
+            curve.Reset();
+        }
+        
+        handleTransform = curve.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+        handleTransform.rotation : Quaternion.identity;
+
+        ShowPoints();
+        oldPoints = curve.points;
+    }
+
+
+    void ShowPoints() {
+        for (int i = 0; i < curve.points.Length; i++) {
+            curve.points[i] = ShowPoint(i);
+        }
+        Handles.color = Color.white;
+        for (int i = 1; i < curve.points.Length; i++) {
+            Handles.DrawLine(curve.points[i-1], curve.points[i]);
+        }
+    }
+
+
+    Vector3 ShowPoint (int index) {
+        Vector3 point = handleTransform.TransformPoint(curve.points[index]);
+        EditorGUI.BeginChangeCheck();
+        point = Handles.DoPositionHandle(point, handleRotation);
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(curve, "MovePoint");
+            EditorUtility.SetDirty(curve);
+            curve.points[index] = handleTransform.InverseTransformPoint(point);
+        }
+        return point;
     }
 }
